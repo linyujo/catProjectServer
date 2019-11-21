83bb11f0416510687537ef4e65b9d9c1dcf3c37c
raine <vikamirror@gmail.com>
2019-11-21 19:42:53 +0800

    [Improve]建立一個後端的Repository

--- src/components/Buttons/UploadButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import uploadButtonStyle from './uploadButtonStyle.css';
-
-const UploadButton = () => <button className="styled-btn button-1">上傳圖片</button>
-
-
-export default UploadButton;
